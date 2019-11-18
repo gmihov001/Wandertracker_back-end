3f3fc23d460c3e47bba442e28ff4d1cf0b18d38d
Fernando Funez <ferfunes@gmail.com>
2019-11-18 23:31:09 +0000

    updates

--- src/models.py
+++ src/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password  = db.Column(db.String(20), unique=True, nullable=False)
-    images = db.relationship('Image', backref="user", lazy=True)
+    
     stamps = db.relationship('Stamp')
 
     def __repr__(self):
@@ -18,9 +18,10 @@ class User(db.Model):
             "username": self.username,
             "email": self.email,
             "password": self.password,
-            "images": list(map(lambda x: x.serialize(), self.images)),
             "stamps": list(map(lambda x: x.serialize(), self.stamps))
         }
+
+        
 class Stamp(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     photo = db.Column(db.String(2000), nullable=False)
