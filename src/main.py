be0ad78cd6fa50533730651a0a6609a2b42407db
Fernando Funez <ferfunes@gmail.com>
2019-11-18 22:52:44 +0000

    tables

--- src/main.py
+++ src/main.py
@@ -8,7 +8,9 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from models import db
-#from models import Person
+from models import User, Stamp
+from flask_jwt_simple import (JWTManager, jwt_required, create_jwt, get_jwt_identity)
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -17,6 +19,33 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
+
+
+# Setup the Flask-JWT-Simple extension
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
+jwt = JWTManager(app)
+# Provide a method to create access tokens. The create_jwt()
+# function is used to actually generate the token
+@app.route('/login', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+    params = request.get_json()
+    username = params.get('username', None)
+    password = params.get('password', None)
+    if not username:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+    # if username != 'test' or password != 'test':
+    #     return jsonify({"msg": "Bad username or password"}), 401
+    usercheck = User.query.filter_by(username=username, password=password).first()
+    if usercheck == None:
+        return jsonify({"msg": "Bad username or password"}), 401
+    # Identity can be any data that is json serializable
+    ret = {'jwt': create_jwt(identity=username)}
+    return jsonify(ret), 200
+
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -28,16 +57,43 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+def handle_users():
+    all_people = User.query.all()
+    all_people = list(map(lambda x: x.serialize(), all_people))
+    return jsonify(all_people), 200
 
-    response_body = {
-        "hello": "world"
-    }
+@app.route('/adduser', methods=['POST'])
+def handle_user():
+    body = request.get_json()
 
-    return jsonify(response_body), 200
+    user1 = User(username=body['username'], email=body['email'], password=body['password'])
+    db.session.add(user1)
+    db.session.commit()
+    return "ok", 200
+
+
+@app.route('/addStamp', methods=['POST'])
+def handle_stamp():
+    body = request.get_json()
+
+        # if body is None:
+        #     raise APIException("You need to specify the request body as a json object", status_code=400)
+        # if 'username' not in body:
+        #     raise APIException('You need to specify the username', status_code=400)
+        # if 'email' not in body:
+        #     raise APIException('You need to specify the email', status_code=400)
+
+    stamp1 = Stamp(user_id=body['user_id'],photo=body['photo'] ,country_value=body['country_value'], country_label=body['country_label'])
+    db.session.add(stamp1)
+    db.session.commit()
+    return "ok", 200
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+
+
--- src/models.py
+++ src/models.py
@@ -2,16 +2,36 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password  = db.Column(db.String(20), unique=True, nullable=False)
+    images = db.relationship('Image', backref="user", lazy=True)
+    stamps = db.relationship('Stamp')
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+    def __repr__(self):
+        return '<User %r>' % self.username
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+    def serialize(self):
+        return {
+            "username": self.username,
+            "email": self.email,
+            "password": self.password,
+            "images": list(map(lambda x: x.serialize(), self.images)),
+            "stamps": list(map(lambda x: x.serialize(), self.stamps))
+        }
+class Stamp(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    photo = db.Column(db.String(2000), nullable=False)
+    country_label = db.Column(db.String(500), nullable=True)
+    country_value = db.Column(db.String(5), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+        nullable=False)
+    def serialize(self):
+        return{
+          "id": self.id,
+          "photo": self.photo,
+          "country_label": self.country_label,
+          "country_value": self.country_value
+        }
